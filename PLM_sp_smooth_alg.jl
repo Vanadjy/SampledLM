0791a7884aca4038e4d36cd44b4d74d427b888cc
vanadjy <valentin.dijon@free.fr>
2024-10-10 15:16:25 -0400

    try PLM very aggressive on bam with 10 runs

--- PLM_sp_smooth_alg.jl
+++ PLM_sp_smooth_alg.jl
@@ -78,8 +78,8 @@ function SPLM(
     Num_mean = 0
     mobile_mean = 0
     unchange_mm_count = 0
-    sample_rates_collec = [.1, .3, .5, .75, 1.0]
-    epoch_limits = [0, 1, 2, 3, 4]
+    sample_rates_collec = [.4, .8, 1.0]
+    epoch_limits = [0, 1, 2]
     @assert length(sample_rates_collec) == length(epoch_limits)
     nls.sample_rate = sample_rate0
 
